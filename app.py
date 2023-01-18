927f5e286a75c276cf34c342f6c4a1ba4468e7a0
xnio94 <xnio94@gmail.com>
2023-01-18 16:34:48 +0100

    1

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    import time
+    print("something")
+    time.sleep(3.5)    # Pause 5.5 seconds
+    print("something")
     return 'Hello, World!'
